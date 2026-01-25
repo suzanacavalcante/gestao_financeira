0401026438abce624f23d290c3466c18322462d0
Suzana Cavalcante <suzanacavalcantepc@gmail.com>
2026-01-25 04:16:03 -0300

    Adição da Funcionalidade de Edição e Exclusão de Categorias            adicionadas

--- djangoapp/accounts/urls.py
+++ djangoapp/accounts/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('cadastro/', views.cadastro, name='cadastro'),
     path('profile/', views.profile, name='profile'),
     path('categorias/', views.categorias, name='categorias'),
+    path('categorias/editar/<int:pk>', views.editar_categoria, name='editar_categoria'),
+    path('categorias/excluir/<int:pk>', views.excluir_categoria, name='excluir_categoria'),
 ]
--- djangoapp/accounts/views.py
+++ djangoapp/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -37,3 +37,24 @@ def categorias(request):
     # Busca apenas as categorias que pertecem ao usuário logado
     lista_categorias = Categoria.objects.filter(user=request.user)
     return render(request, 'accounts/categorias.html', {'form': form, 'categorias': lista_categorias})
+
+@login_required
+def editar_categoria(request, pk):
+    categoria = get_object_or_404(Categoria, pk=pk, user=request.user)
+    if request.method == 'POST':
+        form = CategoriaForm(request.POST, instance=categoria)
+        if form.is_valid():
+            form.save()
+            return redirect('categorias')
+    
+    else:
+        form = CategoriaForm(instance=categoria)
+    return render(request, 'accounts/form_categoria.html', {'form': form, 'titulo':'Editar Categoria'})
+
+@login_required
+def excluir_categoria(request, pk):
+    categoria = get_object_or_404(Categoria, pk=pk, user=request.user)
+    if request.method == 'POST':
+        categoria.delete()
+        return redirect('categorias')
+    return render(request, 'accounts/confirmar_exclusao.html', {'obj': categoria})
